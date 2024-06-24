282d6f7841f552b448c81b1d9e783b52113500d6
lixungeng <lixungeng@corp.wendao.fund>
2024-06-24 14:32:02 +0800

    limit py version

--- src/pyspt.h
+++ src/pyspt.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #if __has_include(<Python.h>)
 #define USE_PYTHON 1
+#define Py_LIMITED_API 0x03080000
 
 // we dont have python310_d.lib, so temporarily disable debug mode
 #pragma push_macro("_DEBUG")
