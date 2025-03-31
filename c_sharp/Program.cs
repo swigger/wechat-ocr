4744c746f4463d0ac520a4386865ef37f17219b2
lixungeng <lixungeng@corp.wendao.fund>
2025-03-31 21:34:08 +0800

    fix cs

--- c_sharp/Program.cs
+++ c_sharp/Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
+using static Program;
 
-class Program
+class WechatOCR
 {
     // 注意要把生成的64位dll拷到 c_sharp\bin\x64\Debug\net8.0 目录下
-    // 声明外部函数
+    delegate void SetResultDelegate(IntPtr result);
+
     [DllImport("wcocr.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool wechat_ocr(
+    static extern bool wechat_ocr(
         [MarshalAs(UnmanagedType.LPWStr)] string ocr_exe,
         [MarshalAs(UnmanagedType.LPWStr)] string wechat_dir,
-        [MarshalAs(UnmanagedType.LPStr)] string imgfn,
+        byte[] imgfn,
         SetResultDelegate set_res);
+
     [DllImport("wcocr.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern void stop_ocr();
 
-    // 定义委托类型
-    public delegate void SetResultDelegate(IntPtr result);
-
-    public class StringResult
+    class StringResult
     {
-        public string result_ = "";
+        string result_ = "";
         public void SetResult(IntPtr dt)
         {
             int length = 0;
@@ -35,6 +35,18 @@ class Program
         }
     };
 
+    public static bool call_wechat_ocr(string ocr_exe, string wechat_dir, string imgfn, out string res)
+    {
+        StringResult stringResult = new StringResult();
+        SetResultDelegate setRes = new SetResultDelegate(stringResult.SetResult);
+        bool success = wechat_ocr(ocr_exe, wechat_dir, Encoding.UTF8.GetBytes(imgfn), setRes);
+        res = stringResult.GetResult();
+        return success;
+    }
+};
+
+class Program
+{
     static void Main(string[] args)
     {
         if (args.Length != 3)
@@ -42,11 +54,9 @@ class Program
             Console.WriteLine("Usage: wcocr.exe ocr_exe wechat_dir imgfn");
             return;
         }
-
-        StringResult stringResult = new StringResult();
-        SetResultDelegate setRes = new SetResultDelegate(stringResult.SetResult);
-        bool success = wechat_ocr(args[0], args[1], args[2], setRes);
-        Console.WriteLine($"OCR Success: {success} res:{stringResult.GetResult()}");
-        stop_ocr();
+        string sRes;
+        bool success = WechatOCR.call_wechat_ocr(args[0], args[1], args[2], out sRes);
+        Console.WriteLine($"OCR Success?:{success} res:{sRes}");
+        WechatOCR.stop_ocr();
     }
 }
